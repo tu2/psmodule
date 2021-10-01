bcc7ab83e77bc53c799262f324d9506dce554d5a
tu2 <7236008+tu2@users.noreply.github.com>
2021-10-01 11:43:01 +0100

    Update psmodule.php

--- psmodule.php
+++ psmodule.php
@@ -1,7 +1,7 @@
 <?php
 
 /*
- * Example module for PrestaShop e-commerce online shop
+ * Skeleton template module for PrestaShop 1.6, 1.7
  * For more information about PrestaShop visit https://www.prestashop.com
  * This code follows the tutorial that can be found at http://doc.prestashop.com/display/PS16/Creating+a+PrestaShop+Module
  *
