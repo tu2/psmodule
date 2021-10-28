192f986ea5a155286a4dd640954da0071662f29d
tu2 <7236008+tu2@users.noreply.github.com>
2021-10-28 11:11:02 +0100

    Update psmodule.php

--- psmodule.php
+++ psmodule.php
@@ -18,23 +18,7 @@ class PsModule extends Module
     public function __construct()
     
     {
-        $this->name = 'psmodule';
-        $this->tab = 'front_office_features';
-        $this->version = '1.0.0';
-        $this->author = 'Firstname Lastname';
-        $this->need_instance = 0;
-        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_); 
-        $this->bootstrap = true;
- 
-        parent::__construct();
- 
-        $this->displayName = $this->l('Ps Module');
-        $this->description = $this->l('Description of Ps Module.');
- 
-        $this->confirmUninstall = $this->l('Are you sure you want to uninstall?');
- 
-        if (!Configuration::get('PSMODULE_NAME'))
-            $this->warning = $this->l('No name provided');
+        //new construct
     }
   
     /*
