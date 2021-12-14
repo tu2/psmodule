ce5bac14b2434b605f78109b1ff154a7250894f7
tu2 <7236008+tu2@users.noreply.github.com>
2021-12-14 10:11:30 +0000

    Update display.php

--- controllers/front/display.php
+++ controllers/front/display.php
@@ -5,6 +5,21 @@
  * Module Version 1.0.0 
 */
 
+class psmoduledisplayModuleFrontController extends ModuleFrontController
+{
+    
+    public function initContent()
+    
+    {
+        parent::initContent();
+        
+        // views/template/front/display.tpl
+        $this->setTemplate('display.tpl');
+        
+    }
+    
+    
+}
 
 
 
