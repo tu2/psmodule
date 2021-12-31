527f64069986ea910bacb7835c3f182ff9fed697
tu2 <7236008+tu2@users.noreply.github.com>
2021-12-31 11:42:27 +0000

    Update index.php

--- img/index.php
+++ img/index.php
@@ -3,25 +3,7 @@
 /*
  * Module Version 1.0.0
  *
- * TODO:
- * Add .htaccess file
- * # Apache 2.2
-
-<IfModule !mod_authz_core.c>
-    Order deny,allow
-    Deny from all
-    <Files ~ "(?i)^.*\.(jpg|jpeg|gif|png|bmp|tiff|svg|pdf|mov|mpeg|mp4|avi|mpg|wma|flv|webm|ico|webp)$">
-        Allow from all
-    </Files>
-</IfModule>
-
-# Apache 2.4
-<IfModule mod_authz_core.c>
-    Require all denied
-    <Files ~ "(?i)^.*\.(jpg|jpeg|gif|png|bmp|tiff|svg|pdf|mov|mpeg|mp4|avi|mpg|wma|flv|webm|ico|webp)$">
-        Require all granted
-    </Files>
-</IfModule>
+ 
 
 */
 
