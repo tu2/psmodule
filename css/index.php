92e629075afd0ac7a9838b4f481796c80662a373
tu2 <7236008+tu2@users.noreply.github.com>
2022-01-02 16:58:02 +0000

    does it work?

--- css/index.php
+++ css/index.php
@@ -2,6 +2,19 @@
 
 /*
  * Module Version 1.0.0
+ *
+ * TODO:
+ * 
+    # Apache 2.2
+    <IfModule !mod_authz_core.c>
+        Order deny,allow
+        Deny from all
+    </IfModule>
+
+    # Apache 2.4
+    <IfModule mod_authz_core.c>
+        Require all denied
+    </IfModule>
 */
 
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
