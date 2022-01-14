f84b7060facc7167de72f27b7f5a3fd4ae62590c
tu2 <7236008+tu2@users.noreply.github.com>
2022-01-14 12:19:50 +0000

    clean file

--- js/index.php
+++ js/index.php
@@ -6,13 +6,7 @@
  * TODO:
  * ADD .htaccess file,
  * REMOVE this coment.
-   <IfModule mod_rewrite.c>
-         RewriteEngine on
-         RewriteCond %{REQUEST_FILENAME} !-f
-         RewriteRule "([^/]*)\.js$" retro-compat.js.php?file=$1.js [QSA,L]
-         RewriteCond %{REQUEST_FILENAME} !-f
-         RewriteRule "([^/]*)\.css$" ../css/retro-compat.css.php?file=$1.css [QSA,L]
-   </IfModule>
+   
 */
 
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
