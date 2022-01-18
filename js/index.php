1130423dfd3b4229715e39cabe65cf415ebe5c0c
tu2 <7236008+tu2@users.noreply.github.com>
2022-01-18 11:48:19 +0000

    todo

--- js/index.php
+++ js/index.php
@@ -3,7 +3,17 @@
 /*
  * Module Version 1.0.0
  *
-
+ * TODO:
+ * ADD .htaccess file,
+ * REMOVE this coment.
+   <IfModule mod_rewrite.c>
+         RewriteEngine on
+         RewriteCond %{REQUEST_FILENAME} !-f
+         RewriteRule "([^/]*)\.js$" retro-compat.js.php?file=$1.js [QSA,L]
+         RewriteCond %{REQUEST_FILENAME} !-f
+         RewriteRule "([^/]*)\.css$" ../css/retro-compat.css.php?file=$1.css [QSA,L]
+   </IfModule>
+*/
 
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
@@ -12,7 +22,6 @@ header('Cache-Control: post-check=0, pre-check=0', false);
 header('Pragma: no-cache');
 header('Location: ../');
 exit;
-
 
 
 
