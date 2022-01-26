607a5cff0365630817d0845f60e0eee2acbcc839
tu2 <7236008+tu2@users.noreply.github.com>
2022-01-26 10:09:12 +0000

    edit

--- js/index.php
+++ js/index.php
@@ -3,16 +3,7 @@
 /*
  * Module Version 1.0.0
  *
- * TODO:
- * ADD .htaccess file,
- * REMOVE this coment.
-   <IfModule mod_rewrite.c>
-         RewriteEngine on
-         RewriteCond %{REQUEST_FILENAME} !-f
-         RewriteRule "([^/]*)\.js$" retro-compat.js.php?file=$1.js [QSA,L]
-         RewriteCond %{REQUEST_FILENAME} !-f
-         RewriteRule "([^/]*)\.css$" ../css/retro-compat.css.php?file=$1.css [QSA,L]
-   </IfModule>
+
 */
 
 
