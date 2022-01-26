452634960c7501258223b8470faff649e829a88d
tu2 <7236008+tu2@users.noreply.github.com>
2022-01-26 10:05:34 +0000

    Update index.php

--- js/index.php
+++ js/index.php
@@ -14,3 +14,12 @@
          RewriteRule "([^/]*)\.css$" ../css/retro-compat.css.php?file=$1.css [QSA,L]
    </IfModule>
 */
+
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0', false);
+header('Pragma: no-cache');
+header('Location: ../');
+exit;
+
