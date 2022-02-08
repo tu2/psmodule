5f7161d159e94198fd1622ca6a9e50c27a87f777
tu2 <7236008+tu2@users.noreply.github.com>
2022-02-08 13:54:06 +0000

    Update index.php

--- controllers/index.php
+++ controllers/index.php
@@ -3,19 +3,7 @@
 /*
  * Module Version 1.0.0
  * 
- * TODO:
- * ADD .htaccess file
- * 
- # Apache 2.2
-   <IfModule !mod_authz_core.c>
-       Order deny,allow
-       Deny from all
-   </IfModule>
 
-   # Apache 2.4
-   <IfModule mod_authz_core.c>
-      Require all denied
-   </IfModule>
 
 */
 
