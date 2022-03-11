2f190b9188610cb257dad205be6fd700750b1324
tu2 <7236008+tu2@users.noreply.github.com>
2022-03-11 10:27:58 +0000

    Update index.php

--- css/index.php
+++ css/index.php
@@ -5,11 +5,7 @@
  *
  * TODO:
  * 
-    # Apache 2.2
-    <IfModule !mod_authz_core.c>
-        Order deny,allow
-        Deny from all
-    </IfModule>
+
     
     # Apache 2.4
     <IfModule mod_authz_core.c>
