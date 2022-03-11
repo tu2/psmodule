855ca7fa0a777fc1f19a1869760ba378470622e8
tu2 <7236008+tu2@users.noreply.github.com>
2022-03-11 10:27:07 +0000

    Update index.php

--- css/index.php
+++ css/index.php
@@ -10,8 +10,11 @@
         Order deny,allow
         Deny from all
     </IfModule>
-
-
+    
+    # Apache 2.4
+    <IfModule mod_authz_core.c>
+        Require all denied
+    </IfModule>
 */
 
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
